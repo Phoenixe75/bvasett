3ec4e22e187b9ebd1799fc9e75f67db7a575e5d0
aliroshan <aliroshan150@gmail.com>
2025-08-08 15:47:59 +0330

    fix card

--- app/components/CardCell/index.tsx
+++ app/components/CardCell/index.tsx
@@ -1,100 +1,102 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import { getAllAdsMain } from '@/app/dashboard/admin/ads/(services)/ads.service';
-import { ProgressSpinner } from 'primereact/progressspinner';
-import { IAdsBase, PurposeEnum } from '@/app/dashboard/admin/ads/(models)/ads';
-import { getLocationLabel, getPurposeLabelwithColor, getTypeLabel } from '@/app/dashboard/admin/ads/constant/converter';
+import React, {useEffect, useState} from 'react';
+import {getAllAdsMain} from '@/app/dashboard/admin/ads/(services)/ads.service';
+import {ProgressSpinner} from 'primereact/progressspinner';
+import {IAdsBase} from '@/app/dashboard/admin/ads/(models)/ads';
 import ItemCard from '../ItemCards/components/ItemCard';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
-import { Button } from 'primereact/button';
-import Image from "next/image";
-import { images } from '@/app/(main)/(pages)/filterResult/(contants)/images';
+import {Button} from 'primereact/button';
 import PropertyDialog from '../PropertyDialog';
 
-export default function CardCell({count, setCount}: {count: number, setCount: React.Dispatch<React.SetStateAction<number>>}) {
-    const [ads, setAds] = useState<IAdsBase[]>([]);
-    const [isLoading, setLoading] = useState<boolean>(true);
-    const [buttonLoading, setButtonLoading] = useState<boolean>(false);
-    const [page, setPage] = useState(1)
-    const [next, setNext] = useState(false);
-    // const [count, setCount] = useState<null | number>(null)
-    const [selectedRowData, setSelectedRowData] = useState<any | null>(null);
-    const [displayDialog, setDisplayDialog] = useState<boolean>(false);
-    const fetchData = async () => {
-        try {
-            setButtonLoading(true);
-            const response = await getAllAdsMain(page);
-            setAds(pre => [...pre, ...response.results]);
-            setCount(response.count)
-            setNext(!!response.next);
-            setPage(pre => pre + 1)
-        } catch (error) {
-            console.error('Error retrieving ads:', error);
-        } finally {
-            setLoading(false);
-            setButtonLoading(false);
-        }
-    };
-    const showDetails = (rowData: any, event: React.MouseEvent) => {
-        event.preventDefault();
-        event.stopPropagation();
-        setSelectedRowData(rowData);
-        setDisplayDialog(true);
-    };
-    const hideDialog = () => {
-        setDisplayDialog(false);
-        setSelectedRowData(null);
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    if (isLoading || ads.length === 0) {
-        return <ProgressSpinner style={{ width: '50px', height: '50px' }} strokeWidth="8" fill="var(--surface-ground)" animationDuration=".5s" />;
+export default function CardCell({count, setCount}: {
+  count: number,
+  setCount: React.Dispatch<React.SetStateAction<number>>
+}) {
+  const [ads, setAds] = useState<IAdsBase[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [buttonLoading, setButtonLoading] = useState<boolean>(false);
+  const [page, setPage] = useState(1)
+  const [next, setNext] = useState(false);
+  // const [count, setCount] = useState<null | number>(null)
+  const [selectedRowData, setSelectedRowData] = useState<any | null>(null);
+  const [displayDialog, setDisplayDialog] = useState<boolean>(false);
+  const fetchData = async () => {
+    try {
+      setButtonLoading(true);
+      const response = await getAllAdsMain(page);
+      setAds(pre => [...pre, ...response.results]);
+      setCount(response.count)
+      setNext(!!response.next);
+      setPage(pre => pre + 1)
+    } catch (error) {
+      console.error('Error retrieving ads:', error);
+    } finally {
+      setLoading(false);
+      setButtonLoading(false);
     }
+  };
+  const showDetails = (rowData: any, event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setSelectedRowData(rowData);
+    setDisplayDialog(true);
+  };
+  const hideDialog = () => {
+    setDisplayDialog(false);
+    setSelectedRowData(null);
+  };
 
-    // const getPurposeLabel = (purpose: PurposeEnum | null): { label: string; className: string; iconClass: string } => {
-    //     switch (purpose) {
-    //         case PurposeEnum.Buy:
-    //             return { label: 'فروش', className: 'bg-yellow-200', iconClass: 'text-yellow-700' };
-    //         case PurposeEnum.Rent:
-    //             return { label: 'اجاره', className: 'bg-cyan-200', iconClass: 'text-cyan-700' };
-    //         case PurposeEnum.Barter:
-    //             return { label: 'معاوضه', className: 'bg-green-200', iconClass: 'text-green-700' };
-    //         case PurposeEnum.MORTGAGE:
-    //             return { label: 'رهن کامل', className: 'bg-green-200', iconClass: 'text-green-700' };
-    //         case PurposeEnum.PARTNERSHIP:
-    //             return { label: 'مشارکت', className: 'bg-green-200', iconClass: 'text-green-700' };
-    //         default:
-    //             return { label: 'نامشخص', className: '', iconClass: '' };
-    //     }
-    // };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <>
-        {/*<div className='flex flex-row-reverse w-full'>*/}
-        {/*    <h5>{`تعداد: ${count}`}</h5>*/}
-        {/*</div>*/}
-        <div className="col-12 p-0 lg:pb-5 mt-4 lg:mt-0 surface-card grid">
-            {ads?.map((item,idx) => {
-                return <div key={idx} className="field col-12 md:col-3"><ItemCard showSelectable={false} onShowDetails={showDetails} data={item} key={item.id} isSelected={false} onClick={()=>{}} selectable={false}/></div>
-            })}
-             {next ? (
-                    <div className="w-full flex justify-content-center align-items-center">
-                        <div>
-                            <Button loading={buttonLoading} onClick={fetchData}>
-                                نمایش بیشتر
-                            </Button>
-                        </div>
-                    </div>
-                ) : null}
-        </div>
-        <PropertyDialog onHide={hideDialog} visible={displayDialog} selectedRowData={selectedRowData}/>
-        </>
-    );
+  if (isLoading || ads.length === 0) {
+    return <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)"
+                            animationDuration=".5s"/>;
+  }
+
+  // const getPurposeLabel = (purpose: PurposeEnum | null): { label: string; className: string; iconClass: string } => {
+  //     switch (purpose) {
+  //         case PurposeEnum.Buy:
+  //             return { label: 'فروش', className: 'bg-yellow-200', iconClass: 'text-yellow-700' };
+  //         case PurposeEnum.Rent:
+  //             return { label: 'اجاره', className: 'bg-cyan-200', iconClass: 'text-cyan-700' };
+  //         case PurposeEnum.Barter:
+  //             return { label: 'معاوضه', className: 'bg-green-200', iconClass: 'text-green-700' };
+  //         case PurposeEnum.MORTGAGE:
+  //             return { label: 'رهن کامل', className: 'bg-green-200', iconClass: 'text-green-700' };
+  //         case PurposeEnum.PARTNERSHIP:
+  //             return { label: 'مشارکت', className: 'bg-green-200', iconClass: 'text-green-700' };
+  //         default:
+  //             return { label: 'نامشخص', className: '', iconClass: '' };
+  //     }
+  // };
+
+  return (
+    <>
+      {/*<div className='flex flex-row-reverse w-full'>*/}
+      {/*    <h5>{`تعداد: ${count}`}</h5>*/}
+      {/*</div>*/}
+      <div className="col-12 p-0 lg:pb-5 mt-4 lg:mt-0 surface-card grid">
+        {ads?.map((item, idx) => {
+          return <div key={idx} className="field col-12 xl:col-3 lg:col-4 sm:col-6"><ItemCard showSelectable={false}
+                                                                            onShowDetails={showDetails} data={item}
+                                                                            key={item.id} isSelected={false}
+                                                                            onClick={() => {
+                                                                            }} selectable={false}/></div>
+        })}
+        {next ? (
+          <div className="w-full flex justify-content-center align-items-center">
+            <div>
+              <Button loading={buttonLoading} onClick={fetchData}>
+                نمایش بیشتر
+              </Button>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <PropertyDialog onHide={hideDialog} visible={displayDialog} selectedRowData={selectedRowData}/>
+    </>
+  );
 }
 // const statusInfo = getPurposeLabel(item.purpose);
 // return (
--- app/components/ItemCards/components/ItemCard/index.tsx
+++ app/components/ItemCards/components/ItemCard/index.tsx
@@ -1,98 +1,103 @@
 'use-client';
 import styles from './ItemCard.module.scss';
 import Image from 'next/image';
-import { getPrice, getPurposeLabelwithColor, getRooms, getTypeLabel } from '@/app/dashboard/admin/ads/constant/converter';
-import { randomPic } from '@/app/(main)/(pages)/filterResult/(contants)/images';
-import { Button } from 'primereact/button';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import classNames from 'classnames';
-import { postFavorite } from '@/app/components/FilterItem/(services)/filter.service';
-import { useUserContext } from '@/layout/context/usercontext';
-import { toast } from 'react-toastify';
-import { TRANSLATIONS } from '@/app/(main)/(pages)/filterResult/translation';
-import { Inquiry, WITHOUT_INQUIRY } from '@/app/components/CardPackage/(models)/package';
-import { formatMoneyToPersianUnit } from '@/app/utils/moneyUtils';
-import { formatDate } from '@/app/utils/dateUtils';
+import {getPrice, getPurposeLabelwithColor, getRooms, getTypeLabel} from '@/app/dashboard/admin/ads/constant/converter';
+import {randomPic} from '@/app/(main)/(pages)/filterResult/(contants)/images';
+import {Button} from 'primereact/button';
+import {useMemo} from 'react';
+import {Inquiry} from '@/app/components/CardPackage/(models)/package';
+import {formatDate} from '@/app/utils/dateUtils';
 import Favorite from './favorite/favorite';
+import { classNames } from 'primereact/utils';
 
 interface ItemCard {
-    isSelected: boolean;
-    onClick?: (data: any) => void;
-    data: any;
-    selectable: boolean;
-    disableMsg?: string;
-    onShowDetails: (data: any, e: any) => void;
-    showSelectable?: boolean;
-    selectedPackage?:Inquiry;
+  isSelected: boolean;
+  onClick?: (data: any) => void;
+  data: any;
+  selectable: boolean;
+  disableMsg?: string;
+  onShowDetails: (data: any, e: any) => void;
+  showSelectable?: boolean;
+  selectedPackage?: Inquiry;
 }
 
 const defaultDisableMessage = 'ابتدا پکیج مورد نظر خود را انتخاب نمایید';
-const ItemCard = ({selectedPackage, data, isSelected, onClick, selectable, onShowDetails, disableMsg = defaultDisableMessage, showSelectable = true }: ItemCard) => {
-    const statusCond = getPurposeLabelwithColor(data.purpose);
-    const getItemPrice = getPrice(data);
+const ItemCard = ({
+                    selectedPackage,
+                    data,
+                    isSelected,
+                    onClick,
+                    selectable,
+                    onShowDetails,
+                    disableMsg = defaultDisableMessage,
+                    showSelectable = true
+                  }: ItemCard) => {
+  const statusCond = getPurposeLabelwithColor(data.purpose);
+  const getItemPrice = getPrice(data);
 
-    const imgSource = useMemo(()=> {
-        if(data.neighborhood_image) {
-            return data.neighborhood_image
-        }
-        return randomPic()
-    },[data])
+  const imgSource = useMemo(() => {
+    if (data.neighborhood_image) {
+      return data.neighborhood_image
+    }
+    return randomPic()
+  }, [data])
 
 
-    return (
-        <article className={styles.wrapper}>
-            <div className={styles.heart_container}>
-             <Favorite data={data}/>
-            </div>
-            <div className={styles.card_info}>
-                <div className={styles.description}>
-                    <span className={'px-2 py-1 chips rounded-1'} style={{ backgroundColor: statusCond.bgColor, color: statusCond.color }}>
+  return (
+    <article className={styles.wrapper}>
+      <div className={styles.heart_container}>
+        <Favorite data={data}/>
+      </div>
+      <div className={styles.card_info}>
+        <div className={styles.description}>
+                    <span className={'px-2 py-1 chips rounded-1'}
+                          style={{backgroundColor: statusCond.bgColor, color: statusCond.color}}>
                         {statusCond.label}
                     </span>
-                </div>
-                <h2 className={styles.title}>
-                    {getTypeLabel(data.type)} {getRooms(data.rooms)}
-                </h2>
-                <div className={styles.description}>
-                    <b>{formatDate(data.created)}</b>
-                </div>
-                <div className={styles.description}>
-                    {Math.round(+data.area!)} متر {data?.neighborhood_name}
-                </div>
-                <div className={styles.description}>
-                    {getItemPrice.rent ? 'رهن: ' : null}
-                    {getItemPrice.total}
-                </div>
-                {getItemPrice.rent ? <div className={styles.description}>اجاره: {getItemPrice.rent}</div> : null}
+        </div>
+        <h2 className={classNames(styles.title, {})}>
+          {getTypeLabel(data.type)} {getRooms(data.rooms)}
+        </h2>
+        <div className={styles.description}>
+          <b>{formatDate(data.created)}</b>
+        </div>
+        <div className={styles.description}>
+          {Math.round(+data.area!)} متر {data?.neighborhood_name}
+        </div>
+        <div className={styles.description}>
+          {getItemPrice.rent ? 'رهن: ' : null}
+          {getItemPrice.total}
+        </div>
+        {getItemPrice.rent ? <div className={styles.description}>اجاره: {getItemPrice.rent}</div> : null}
 
-                <div className={styles.action_wrapper}>
-                    {showSelectable && (
-                        <div className="pointer">
-                            {/* <InputSwitch tooltipOptions={{ position: 'bottom', showOnDisabled: true }} tooltip={!selectable ? disableMsg : ''} disabled={!selectable} checked={isSelected} onChange={(e) => onClick(data)} /> */}
-                            <Button
-                                tooltipOptions={{ position: 'bottom', showOnDisabled: true }}
-                                tooltip={!selectable ? disableMsg : ''}
-                                size="small"
-                                severity={isSelected ? 'danger' : 'success'}
-                                onClick={(e) => {
-                                    onClick?.(data);
-                                }}
-                            >
-                                {isSelected ? 'عدم‌انتخاب' : 'انتخاب'}
-                            </Button>
-                        </div>
-                    )}
-                    {/* <i className="pi pi-eye text-yellow-500 pointer" style={{ fontSize: '1.5rem' }} onClick={(e) => onShowDetails(data, e)}></i> */}
-                    <div className="w-full">
-                        <Button raised size="small" severity="success" className="pointer w-full justify-content-center"
-                                onClick={(e) => onShowDetails(data, e)}>
-                            جزئیات
-                        </Button>
-                    </div>
-                </div>
+        <div className={styles.action_wrapper}>
+          {showSelectable && (
+            <div className="pointer">
+              {/* <InputSwitch tooltipOptions={{ position: 'bottom', showOnDisabled: true }} tooltip={!selectable ? disableMsg : ''} disabled={!selectable} checked={isSelected} onChange={(e) => onClick(data)} /> */}
+              <Button
+                tooltipOptions={{position: 'bottom', showOnDisabled: true}}
+                tooltip={!selectable ? disableMsg : ''}
+                size="small"
+                severity={isSelected ? 'danger' : 'success'}
+                onClick={(e) => {
+                  onClick?.(data);
+                }}
+              >
+                {isSelected ? 'عدم‌انتخاب' : 'انتخاب'}
+              </Button>
             </div>
-            <div className={styles.card_thumbnail}><Image src={imgSource} alt="img" width={130} height={130} /> </div>
-        </article>
-    );
+          )}
+          {/* <i className="pi pi-eye text-yellow-500 pointer" style={{ fontSize: '1.5rem' }} onClick={(e) => onShowDetails(data, e)}></i> */}
+          <div className="w-full">
+            <Button raised size="small" severity="success" className="pointer w-full justify-content-center"
+                    onClick={(e) => onShowDetails(data, e)}>
+              جزئیات
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.card_thumbnail}><Image src={imgSource} alt="img" width={130} height={130}/></div>
+    </article>
+  );
 };
 export default ItemCard;
